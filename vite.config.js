458a860416626036d3afc4045081c4b2f83c8bf2
Brayden Atkinson <bma2026@iastate.edu>
2025-12-09 17:04:10 -0600

    update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/cpre-4310-final/',
   build: {
     outDir: 'docs',
   },
